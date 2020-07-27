ab2c962b755c61517597cc962855678f7d496bfe
trandinhnam <namtd72@wru.vn>
2020-07-27 09:42:33 +0700

    configuration

---
+++ DemoShop.Data/EF/DemoShopDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using DemoShop.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<DemoShopDbContext>
+    {
+        public DemoShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DemoShopDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<DemoShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new DemoShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
